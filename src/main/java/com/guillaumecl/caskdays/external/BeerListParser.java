232d5fabf01742007e6642ed8b1291abdd44335a
gcl <gcl@bigvikinggames.com>
2015-10-07 17:59:07 -0400

    Initial try at a beer name spell checker.

--- src/main/java/com/guillaumecl/caskdays/external/BeerListParser.java
+++ src/main/java/com/guillaumecl/caskdays/external/BeerListParser.java
@@ -2,6 +2,7 @@ package com.guillaumecl.caskdays.external;
 
 import com.guillaumecl.caskdays.Config;
 import com.guillaumecl.caskdays.models.Beer;
+import com.guillaumecl.caskdays.spellcheck.SpellChecker;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
@@ -44,12 +45,18 @@ public class BeerListParser {
 	private List<Beer> beerList;
 	
 	/**
+	 * The spell checker of the beer names.
+	 */
+	private final SpellChecker spellChecker;
+	
+	/**
 	 * Construct a beer list parser.
 	 * 
 	 * @param config 
 	 */
 	public BeerListParser(Config config) {
 		this.config = config;
+		spellChecker = config.getSpellChecker();
 		initialized = false;
 		beerList = Collections.emptyList();
 	}
@@ -113,9 +120,10 @@ public class BeerListParser {
 	private List<Beer> getBeersFromSection(Element section) {
 		List<Beer> sectionBeers = section.select("div.menu-item").stream().map(element -> {
 			String name = element.select("div.menu-item-title").text();
+			String fixedName = spellChecker.correctPhrase(name);
 			String style = element.select("div.menu-item-description").text();
 			Beer beer = new Beer();
-			beer.setName(name);
+			beer.setName(fixedName);
 			beer.setStyle(style);
 			return beer;
 		}).collect(Collectors.toList());
@@ -170,7 +178,7 @@ public class BeerListParser {
 						return beer;
 					});
 				})
-				//.filter(beer -> ! StringUtils.equalsIgnoreCase(beer.getName(), "to be announced"))
+				.filter(beer -> ! StringUtils.equalsIgnoreCase(beer.getName(), "to be announced"))
 				.collect(Collectors.toList());
 		
 		return beers;
