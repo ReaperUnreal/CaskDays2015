e646513981278d5a8767f7772c10387fc5d0364e
gcl <gcl@bigvikinggames.com>
2015-10-21 23:30:15 -0400

    Add checkboxes for each beer.

--- client/main.js
+++ client/main.js
@@ -1,5 +1,8 @@
+var beerList = [];
+
 $.get('http://159.203.19.50:8081/beers', function gotData(data) {
 	console.debug('got data: ', data);
+	beerList = data;
 	createTable(data);
 }, 'json');
 
@@ -11,13 +14,24 @@ function createTable(beerList) {
 	$('<table/>', {
 		id: 'beerListTable'
 	}).addClass('table table-hover tablesorter').appendTo('div#main');
-	$('#beerListTable').append('<thead><tr><th>Name</th><th>Style</th><th>Region</th></thead>');
+	var table = $('#beerListTable');
+	table.append('<thead><tr><th>Name</th><th>Style</th><th>Region</th><th>Chosen</th></thead>');
 
 	// create a new row for each beer
-	_.forEach(beerList, function createBeerRow(beer) {
-		var str = '<tr><td>' + beer.name + '</td><td>' + beer.style + '</td><td>' + beer.region + '</td></tr>';
-		$('#beerListTable').append(str);
+	_.forEach(beerList, function createBeerRow(beer, idx) {
+		var row = $('<tr/>');
+		$('<td/>').text(beer.name).appendTo(row);
+		$('<td/>').text(beer.style).appendTo(row);
+		$('<td/>').text(beer.region).appendTo(row);
+		var chosen = $('<td/>').addClass('text-center');
+		$('<input />', {
+			type: 'checkbox',
+			id: 'beerCb' + idx,
+			value: idx
+		}).appendTo(chosen);
+		chosen.appendTo(row);
+		table.append(row);
 	});
 
-	$('#beerListTable').tablesorter();
+	table.tablesorter();
 }
