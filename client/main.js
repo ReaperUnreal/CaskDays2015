7f6eb9fdabe8a339d7a882b26180ee018c40de3e
gcl <gcl@bigvikinggames.com>
2015-10-22 21:43:18 -0400

    Cache bust the beer list.

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ var beerList = [];
 
 var LOCAL_STORAGE_KEY = 'caskdays2015chosen';
 
-$.get('http://159.203.19.50:8081/beers', function gotData(data) {
+$.get('http://159.203.19.50:8081/beers?cacheBust=' + Date.now(), function gotData(data) {
 	console.debug('got data: ', data);
 	beerList = data;
 	createTable(data);
