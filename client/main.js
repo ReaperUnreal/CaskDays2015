4fa490ce4574939c39ff12b196754b8b3e4e14c8
gcl <gcl@bigvikinggames.com>
2018-10-14 20:39:48 -0400

    Add a bunch of usability changes.

--- client/main.js
+++ client/main.js
@@ -15,19 +15,31 @@ function toggleViewingChosen() {
 	isViewingChosen = ! isViewingChosen;
 	$('span#toggle-favs-icon').toggleClass('glyphicon-star');
 	$('span#toggle-favs-icon').toggleClass('glyphicon-star-empty');
+	$('span#toggle-favs-text').text(isViewingChosen ? "Show All" : "Show Chosen Only");
 }
 var favs = $('button#toggle-favs');
 favs.on('click', toggleViewingChosen);
 
 function copyShareUrl() {
 	var chosenIds = _.keys(chosenById);
+	var query;
 	if (! chosenIds.length) {
-		return;
+		query = 'https://caskdayshelper.com/';
+	} else {
+		query = 'https://caskdayshelper.com/?' + chosenIds.join('&');
 	}
-	var query = '/?' + chosenIds.join('&');
+
+	swal({
+		title: "Share Link",
+		text: "Copy and paste this text to share with others.",
+		type: "info",
+		input: "text",
+		inputValue: query,
+		showCancelButton: false
+	});
 	// TOOD: just write to clipboard
 	// rather than mess with location
-	history.replaceState({}, 'chosen', query);
+	//history.replaceState({}, 'chosen', query);
 }
 var share = $('button#share-url');
 share && share.on('click', copyShareUrl);
@@ -37,6 +49,7 @@ function toggleViewingSession3() {
 	shouldRemoveSession3 = ! shouldRemoveSession3;
 	$('button#toggle-session3').toggleClass('btn-danger');
 	$('button#toggle-session3').toggleClass('btn-warning');
+	$('span#toggle-session3-text').text(shouldRemoveSession3 ? "Show Session 3" : "Hide Session 3");
 }
 var session3 = $('button#toggle-session3');
 session3.on('click', toggleViewingSession3);
@@ -46,6 +59,7 @@ function toggleViewingKegs() {
 	shouldRemoveKegs = ! shouldRemoveKegs;
 	$('button#toggle-keg').toggleClass('btn-danger');
 	$('button#toggle-keg').toggleClass('btn-warning');
+	$('span#toggle-keg-text').text(shouldRemoveKegs ? "Show Kegs" : "Hide Kegs");
 }
 var kegs = $('button#toggle-keg');
 kegs.on('click', toggleViewingKegs);
@@ -86,6 +100,17 @@ function shortenRegion(region) {
 	}
 }
 
+var headerNames = [
+	"No.",
+	"Brewery",
+	"Name",
+	"Style",
+	"ABV",
+	"Region",
+	"Session",
+	"Chosen"
+];
+
 function createTable(beerList, favoritesOnly, removeSession3, removeKegs) {
 	// update chosen
 	loadChosenFromLocalStorage();
@@ -99,7 +124,15 @@ function createTable(beerList, favoritesOnly, removeSession3, removeKegs) {
 	}).addClass('table table-hover tablesorter').appendTo('div#main');
 	var table = $('#beerListTable');
 	var thead = $('<thead />').addClass('noprint').appendTo(table);
-	thead.append('<tr><th>ID</th><th>Session</th><th>Brewery</th><th>Name</th><th>Style</th><th>ABV</th><th>Region</th><th>Chosen</th>');
+	var headerRow = $('<tr />');
+	var sortIcon = $('<span />').addClass('noprint').addClass('glyphicon').addClass('glyphicon-sort');
+	var numHeaders = headerNames.length;
+	for (var idxHeader = 0; idxHeader < numHeaders; idxHeader++) {
+		var headerName = headerNames[idxHeader];
+		$('<th />').attr("id", "header-id-" + idxHeader).text(headerName).appendTo(headerRow);
+	}
+	$('<th />').append(sortIcon).appendTo(headerRow);
+	thead.append(headerRow);
 
 	// create a new row for each beer
 	_.forEach(beerList, function createBeerRow(beer, idx) {
@@ -134,12 +167,12 @@ function createTable(beerList, favoritesOnly, removeSession3, removeKegs) {
 			abv = "";
 		}
 		$('<td/>').addClass('printable').text(beer.id).appendTo(row);
-		$('<td/>').addClass('printable').text("" + session).appendTo(row);
 		$('<td/>').addClass('printable').text(beer.brewery).appendTo(row);
 		$('<td/>').addClass('printable').text(beer.name).appendTo(row);
 		$('<td/>').addClass('printable').text(beer.style).appendTo(row);
 		$('<td/>').addClass('printable').text(abv).appendTo(row);
 		$('<td/>').addClass('printable').text(region).appendTo(row);
+		$('<td/>').addClass('printable').text("" + session).appendTo(row);
 		var chosen = $('<td/>').addClass('printable');
 		var icon = $('<span/>');
 		if (beer.chosen) {
@@ -155,7 +188,11 @@ function createTable(beerList, favoritesOnly, removeSession3, removeKegs) {
 			var chosen = beer.chosen = ! beer.chosen;
 			// this is awful but overwrite history
 			// to remove any stale share url query params
-			history.replaceState({}, 'chosen', '/');
+			try {
+				history.replaceState({}, 'chosen', '/');
+			} catch (err) {
+				console.error("RIP: ", err);
+			}
 
 			if (chosen) {
 				chosenById[beer.id] = beer;
@@ -221,5 +258,6 @@ if ((/^\?/).test(query)) {
 }
 
 // meh sure, just flog local storage twice
-// TODO: kill all of this with fire before caskdays 2018
+// TODO: kill all of this with fire before caskdays 2019... lol
 createTable(beerList);
+$("th#header-id-0").click();
